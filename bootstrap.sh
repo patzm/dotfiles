d29e9cd10905aaf34f10b3cade7c04af88f3baa9
Martin Patz <mailto@martin-patz.de>
2017-08-16 16:31:18 +0200

    added install script

---
+++ bootstrap.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+pushd `dirname $0` > /dev/null
+DIR=`pwd`
+popd > /dev/null
+
+echo "Location of the configs repository: $DIR"
+
+program_installed() {
+    if hash "$1" 2>/dev/null; then
+        return 0 # true
+    else
+        echo "$1 does not seem to be installed"
+        return 1 # false
+    fi
+}
+
+# i3
+if program_installed(i3); then
+    ln -sf $DIR/i3 ~/.i3
+fi
+
+# vim
+if program_installed(vim); then 
+    ln -sf $DIR/vim/.vimrc ~/.vimrc
+fi
+
+# zsh
+if program_installed(zsh); then
+    ln -sf $DIR/ZSH/.zshrc ~/.zshrc
+fi
+
