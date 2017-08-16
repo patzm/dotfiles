6fd01ffdb07e78da0a77adcc4ca74500c9c08350
Martin Patz <mailto@martin-patz.de>
2017-08-16 16:58:06 +0200

    added tmux script, complemented boostrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,17 +16,24 @@ program_installed() {
 }
 
 # i3
-if program_installed(i3); then
+if program_installed "i3"; then
     ln -sf $DIR/i3 ~/.i3
 fi
 
 # vim
-if program_installed(vim); then 
+if program_installed "vim"; then 
     ln -sf $DIR/vim/.vimrc ~/.vimrc
 fi
 
 # zsh
-if program_installed(zsh); then
+if program_installed "zsh"; then
     ln -sf $DIR/ZSH/.zshrc ~/.zshrc
+    echo "Changing the default log-in shell to zsh"
+    chsh -s $(which zsh)
+fi
+
+# tmux
+if program_installed "tmux"; then
+    ln -sf $DIR/tmux/.tmux.conf ~/.tmux.conf
 fi
 
