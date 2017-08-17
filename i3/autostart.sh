435f1669018c889fd4aa89bc5221c743c5dfbab2
Martin Patz <mailto@martin-patz.de>
2017-08-17 16:48:56 +0200

    added autostart script to the i3 configuration

---
+++ i3/autostart.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+program_exists() {
+    if hash $1 2>/dev/null; then
+        return 0 # true
+    else
+        return 1 # false
+    fi
+}
+
+if program_exists "dropbox"; then
+    dropbox start
+fi
+
