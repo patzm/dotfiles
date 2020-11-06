2df2ee6637182b3455fe15963ab9fb6bcaa22891
Martin Patz <mailto@martin-patz.de>
2020-11-06 11:11:09 +0100

    fix polybar launch when other users run it as well, closes #57

--- roles/polybar/files/launch.sh
+++ roles/polybar/files/launch.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 # Terminate already running bar instances
-killall -q polybar
+killall -u ${USER} -q polybar
 
 # Wait until the processes have been shut down
-while pgrep -x polybar >/dev/null; do sleep 1; done
+while pgrep -u ${USER} -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
 nohup polybar topbar > /dev/null 2>&1 &
