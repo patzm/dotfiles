976d2dcde08702485cd4e42eea68bfab96450640
Martin Patz <mailto@martin-patz.de>
2024-01-02 17:53:27 +0100

    prevent the back and forth swipe gestures

--- roles/firefox/files/user.js
+++ roles/firefox/files/user.js
@@ -1,5 +1,7 @@
 // Custom user-settings
 user_pref("browser.fullscreen.exit_on_escape", false);
+user_pref("browser.gesture.swipe.left", "");
+user_pref("browser.gesture.swipe.right", "");
 user_pref("browser.tabs.closeWindowWithLastTab", false);
 user_pref("mousewheel.with_control.action", 1);
 user_pref("places.frecency.bookmarkVisitBonus", 1000);
