47619ce0fdb0136512d4152c537be6b75bf268d1
Martin Patz <mailto@martin-patz.de>
2024-01-02 17:51:24 +0100

    sort lines

--- roles/firefox/files/user.js
+++ roles/firefox/files/user.js
@@ -1,8 +1,8 @@
 // Custom user-settings
 user_pref("browser.fullscreen.exit_on_escape", false);
 user_pref("browser.tabs.closeWindowWithLastTab", false);
-user_pref("privacy.webrtc.legacyGlobalIndicator", false);
-user_pref("privacy.webrtc.hideGlobalIndicator", true);
 user_pref("mousewheel.with_control.action", 1);
 user_pref("places.frecency.bookmarkVisitBonus", 1000);
 user_pref("places.frecency.unvisitedBookmarkBonus", 1000);
+user_pref("privacy.webrtc.hideGlobalIndicator", true);
+user_pref("privacy.webrtc.legacyGlobalIndicator", false);
