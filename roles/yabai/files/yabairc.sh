fdde3941494851ab295446a44625074524652c42
Martin Patz <martin@patz.family>
2024-09-04 12:37:13 +0200

    improve current window visibility

--- roles/yabai/files/yabairc.sh
+++ roles/yabai/files/yabairc.sh
@@ -17,6 +17,12 @@ yabai -m config left_padding   3
 yabai -m config right_padding  3
 yabai -m config window_gap     5
 
+# render all unfocused windows with 90% opacity
+yabai -m config window_shadow float
+yabai -m config window_opacity on
+yabai -m config active_window_opacity 1.0
+yabai -m config normal_window_opacity 0.9
+
 # on or off (default: off)
 yabai -m config auto_balance off
 
