5f28c3ecb146a4b5090875c044b41cd4e690c261
Martin Patz <martin@patz.family>
2024-09-23 14:31:19 -0600

    Bitwarden master-password prompts are floating

--- roles/yabai/files/yabairc.sh
+++ roles/yabai/files/yabairc.sh
@@ -47,5 +47,6 @@ yabai -m rule --add app="^nextcloud$" manage=off
 yabai -m rule --add app="^System Settings$" manage=off
 yabai -m rule --add app="iStat Menus.*" manage=off mouse_follows_focus=off
 yabai -m rule --add app="ONLYOFFICE" manage=on
+yabai -m rule --add app="^Firefox$" title=".*Bitwarden Password Manager.*$" manage=off
 
 echo "yabai configuration loaded."
