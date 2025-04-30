17717bcb379c37e9b28b105c37215cf25e0fdfaa
Martin Patz <martin@patz.family>
2025-04-30 12:26:10 +0200

    enable Firefox profiles by default

--- roles/firefox/files/user.js
+++ roles/firefox/files/user.js
@@ -2,6 +2,7 @@
 user_pref("browser.fullscreen.exit_on_escape", false);
 user_pref("browser.gesture.swipe.left", "");
 user_pref("browser.gesture.swipe.right", "");
+user_pref("browser.profiles.enabled", true);
 user_pref("browser.tabs.closeWindowWithLastTab", false);
 user_pref("mousewheel.with_control.action", 1);
 user_pref("places.frecency.bookmarkVisitBonus", 1000);
