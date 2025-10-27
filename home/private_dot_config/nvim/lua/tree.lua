bd104d35a2c0d2873f17ed23a6c6811f59bfaab3
Martin Patz <martin@patz.family>
2025-10-28 00:12:39 +0100

    no need for this

--- home/private_dot_config/nvim/lua/tree.lua
+++ home/private_dot_config/nvim/lua/tree.lua
@@ -4,8 +4,6 @@ local nvim_tree = require("nvim-tree")
 -- vim.g.loaded_netrw = 1
 -- vim.g.loaded_netrwPlugin = 1
 
--- vim.opt.termguicolors = true
-
 nvim_tree.setup {
   sort = {
     sorter = "case_sensitive",
